ae41fcc9d99954d8185e3c9c1396af73f2c5d7b3
pengpeng_fu <pengpeng_fu@infinova.com.cn>
2024-01-24 13:26:50 +0800

    fix condition judge equals

--- code-core/src/main/java/io/github/bigbird0101/code/core/template/ConditionJudgeSupport.java
+++ code-core/src/main/java/io/github/bigbird0101/code/core/template/ConditionJudgeSupport.java
@@ -178,7 +178,7 @@ public class ConditionJudgeSupport {
                                 .noneMatch(s -> s.equals(String.valueOf(temp2)));
                         stack.push(String.valueOf(anyMatch));
                     }else {
-                        stack.push(String.valueOf(!StrUtil.contains(String.valueOf(temp2), String.valueOf(temp1))));
+                        stack.push(String.valueOf(!StrUtil.contains(String.valueOf(temp1), String.valueOf(temp2))));
                     }
                 }
             }else if("in".equals(value)){
@@ -201,7 +201,7 @@ public class ConditionJudgeSupport {
                                 .anyMatch(s -> s.equals(String.valueOf(temp2)));
                         stack.push(String.valueOf(anyMatch));
                     }else {
-                        stack.push(String.valueOf(StrUtil.contains(String.valueOf(temp2), String.valueOf(temp1))));
+                        stack.push(String.valueOf(StrUtil.contains(String.valueOf(temp1), String.valueOf(temp2))));
                     }
                 }
             }
